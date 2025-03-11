a918667bbec7330fcf305643763583c903bb8de4
whoNann <mralbaqiananta@gmail.com>
2025-03-11 09:12:01 +0700

    menambahkan implementasi dengan table-driven

---
+++ 04_OOP/Jurnal_OOP_2311104005/KodeBuah.js
@@ -0,0 +1,23 @@
+class KodeBuah {
+    constructor() {
+        this.kodeBuahMap = new Map([
+            ["Apel", "A00"],
+            ["Aprikot", "B00"],
+            ["Alpukat", "C00"],
+            ["Pisang", "D00"],
+            ["Paprika", "E00"],
+            ["Kurma", "K00"],
+            ["Durian", "L00"],
+            ["Anggur", "M00"],
+            ["Melon", "N00"],
+            ["Semangka", "O00"]
+        ]);
+    }
+
+    getKodeBuah(namaBuah) {
+        return this.kodeBuahMap.get(namaBuah) || "Kode tidak ditemukan";
+    }
+}
+
+const kodeBuah = new KodeBuah();
+console.log(`Kode Buah Apel: ${kodeBuah.getKodeBuah("Apel")}`);
