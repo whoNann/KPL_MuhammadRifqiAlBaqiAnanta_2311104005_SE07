cf3cf1d724a77c0e94a820576efe7fe8d6361cbb
whoNann <mralbaqiananta@gmail.com>
2025-03-20 00:35:10 +0700

    update KodeBuah class to modify Aprikot code and add new fruit entries; enhance console output for fruit list and specific fruit code retrieval

--- 04_OOP/Jurnal_OOP_2311104005/KodeBuah.js
+++ 04_OOP/Jurnal_OOP_2311104005/KodeBuah.js
@@ -2,10 +2,14 @@ class KodeBuah {
     constructor() {
         this.kodeBuahMap = new Map([
             ["Apel", "A00"],
-            ["Aprikot", "B00"],
+            ["Aprikot", "A01"],
             ["Alpukat", "C00"],
             ["Pisang", "D00"],
             ["Paprika", "E00"],
+            ["Blackberry", "F00"],
+            ["Ceri", "G00"],
+            ["Kelapa", "H00"],
+            ["Jagung", "I00"],
             ["Kurma", "K00"],
             ["Durian", "L00"],
             ["Anggur", "M00"],
@@ -20,7 +24,10 @@ class KodeBuah {
 }
 
 const kodeBuah = new KodeBuah();
-console.log(`Kode Buah Apel: ${kodeBuah.getKodeBuah("Apel")}`);
 
+console.log("Daftar Nama Buah:");
+console.log("Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka");
 
-// ubah message
+const namaBuah = "Ceri"; 
+console.log(`Masukkan nama buah: ${namaBuah}`);
+console.log(`Kode Buah ${namaBuah}: ${kodeBuah.getKodeBuah(namaBuah)}`);
